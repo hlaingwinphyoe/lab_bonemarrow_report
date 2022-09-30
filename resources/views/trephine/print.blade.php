55e3e62d33e715d8d5a4bc0c525fac2e19ab920c
Hlaing Win Phyoe <99072872+hlaingwinphyoe@users.noreply.github.com>
2022-09-30 10:15:38 +0630

    Update print.blade.php

--- resources/views/trephine/print.blade.php
+++ resources/views/trephine/print.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $patientFact->patient_name }} : Trephine Report</title>
     <link rel="stylesheet" href="{{ asset('css/theme.css') }}">
-    <link rel="icon" href="{{ asset('images/logo.jpg') }}">
+    <link rel="icon" href="{{ asset('images/logo.png') }}">
     <style>
         .print-table tr{
             font-size: 12px !important;
