14735e47284e6d59ab76dc017129a15ee9bb5c18
Hlaing Win Phyoe <99072872+hlaingwinphyoe@users.noreply.github.com>
2022-09-30 10:14:04 +0630

    Update print.blade.php

--- resources/views/histo/print.blade.php
+++ resources/views/histo/print.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $patientFact->name }} : Histo Report</title>
     <link rel="stylesheet" href="{{ asset('css/theme.css') }}">
-    <link rel="icon" href="{{ asset('images/logo.jpg') }}">
+    <link rel="icon" href="{{ asset('images/logo.png') }}">
     <style>
         .print-header span{
             font-size: 15px !important;
